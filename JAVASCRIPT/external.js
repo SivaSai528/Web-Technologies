dba7a5a3a5363358fed278861c1abc2c648d49a6
Siva <110374593+SivaSai528@users.noreply.github.com>
2023-02-19 17:04:07 +0530

    Add files via upload

---
+++ JAVASCRIPT/external.js
@@ -0,0 +1,12 @@
+function rectangle() 
+{ 
+    alert("Area= Length*Breadth"); 
+} 
+function triangle() 
+{
+    alert("Area=0.5*Base*Perpendicular_Height"); 
+} 
+function circle()  
+{ 
+    alert("Area=Pie*Radius*Radius");  
+} 
---
+++ JAVASCRIPT/external2.js
@@ -0,0 +1,8 @@
+function hide() 
+{
+    document.getElementById('hide').style.display='none'
+}
+function size() 
+{
+    document.getElementById('size').style.fontSize='35px'
+}
